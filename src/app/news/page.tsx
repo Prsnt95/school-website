e487e3986594dec9305628ac18e10cceeb14f83d
Prashant Ghimire <111095724+Prsnt95@users.noreply.github.com>
2025-06-17 23:32:53 -0500

    news placeholders

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 const news = () => {
   return (
-    <div className='h-full'>
-      <div className='flex justify-end items-center bg-secondary h-20 px-20'>
+    <div className='h-full flex flex-col items-center '>
+      <div className='flex justify-end items-center bg-secondary h-20 !px-20 w-1/1'>
         <h1 className='text-white text-4xl font-bold'>News</h1>
+      </div>
+      <div className='flex flex-row flex-wrap  bg-amber-100 w-4/5 h 4/5 !my-5'>
+        <div className='flex  h-30 w-30 bg-amber-500 !m-20 '></div>
+        <div className='flex   h-30 w-30 bg-amber-500 !m-20 '></div>
+        <div className='flex  h-30 w-30 bg-amber-500 !m-20 '></div>
+        <div className='flex   h-30 w-30 bg-amber-500 !m-20 '></div>
+        <div className='flex  h-30 w-30 bg-amber-500 !m-20 '></div>
+        <div className='flex   h-30 w-30 bg-amber-500 !m-20 '></div>
       </div>
     </div>
   );
