56a5e6778d9b16a72a8367ed40b07fd084e512cc
Prashant Ghimire <111095724+Prsnt95@users.noreply.github.com>
2025-06-20 21:45:25 -0500

    checkin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' className='h-full'>
-      <body className='h-full flex flex-col'>
+      <body className='h-full flex flex-col antialiased overflow-x-hidden'>
         <Navbar />
         <main className='flex-grow flex flex-col'>{children}</main>
       </body>
