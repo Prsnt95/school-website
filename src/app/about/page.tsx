d0105ef25e138bec84ddd2bcdd80237664daf52c
Prashant Ghimire <111095724+Prsnt95@users.noreply.github.com>
2025-06-28 17:45:33 +0100

    text fix

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -40,7 +40,7 @@ export default function About() {
                 <path d='M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z' />
               </svg>
               <blockquote className='text-2xl md:text-3xl font-light text-gray-700 leading-relaxed mb-6 relative'>
-                Something wise some wise guy (or girl) said.
+                Something wise some wise guy said.
                 <svg
                   className='absolute -bottom-2 -right-4 w-12 h-12 text-primary/20 rotate-180'
                   fill='currentColor'
@@ -50,7 +50,7 @@ export default function About() {
                 </svg>
               </blockquote>
               <cite className='text-lg text-primary  font-medium'>
-                — Some Wise Guy (JK deffo a guy)
+                — Some Wise Guy
               </cite>
             </div>
           </div>
